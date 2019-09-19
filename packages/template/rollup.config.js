617ebda051eab3b361e1fbfe8fa154f7b43749dc
Jungle <devex.soft@gmail.com>
2019-09-19 17:13:07 +0500

    feat(template): Update template dependencies

--- packages/template/rollup.config.js
+++ packages/template/rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 
@@ -47,12 +48,23 @@ export default {
     // some cases you'll need additional configuration —
     // consult the documentation for details:
     // https://github.com/rollup/rollup-plugin-commonjs
-    resolve(),
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
+    }),
     commonjs(),
     typescript(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser()
-  ]
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
--- packages/template/src/App.svelte
+++ packages/template/src/App.svelte
@@ -1,9 +1,21 @@
-<h1>Hello {name}!</h1>
-<p>
-  <Counter />
-  <Counter value={1}>Counter 1</Counter>
-  <Counter value={$count} step={3}>Counter 2</Counter>
-</p>
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import { writable } from "svelte/store";
+
+  import Counter from "./Counter.svelte";
+
+  const count = writable(100);
+  const unsubscribe = count.subscribe(value => console.log("count", value));
+
+  export let name: string;
+  onMount(() => {
+    console.log("App mounted");
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+</script>
 
 <style>
   h1 {
@@ -11,16 +23,9 @@
   }
 </style>
 
-<script lang="ts">
-  import { onMount } from "svelte";
-  import { writable } from "svelte/store";
-
-  import Counter from "./Counter.svelte";
-
-  const count = writable(100);
-
-  export let name: string;
-  onMount(() => {
-    console.log("App mounted");
-  });
-</script>
+<h1>Hello {name}!</h1>
+<p>
+  <Counter />
+  <Counter value={1}>Counter 1</Counter>
+  <Counter bind:value={$count} step={3}>Counter 2</Counter>
+</p>
