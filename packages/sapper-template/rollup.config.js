8f25ce479b7268beec31fd8bc59e89aaf646b344
Jungle <devex.soft@gmail.com>
2020-01-27 16:50:13 +0500

    build(rollup, sapper-template): Replace rollup-plugin-typescript2 to @wessberg/rollup-plugin-ts

--- packages/sapper-template/rollup.config.js
+++ packages/sapper-template/rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from "rollup-plugin-commonjs";
 import svelte from "rollup-plugin-svelte";
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@wessberg/rollup-plugin-ts";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
 
@@ -17,102 +17,102 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-    (warning.code === "CIRCULAR_DEPENDENCY" &&
-        /[/\\]@sapper[/\\]/.test(warning.message)) ||
-    onwarn(warning);
+  (warning.code === "CIRCULAR_DEPENDENCY" &&
+    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning);
 
 export default {
-    client: {
-        input: config.client.input(),
-        output: config.client.output(),
-        plugins: [
-            replace({
-                "process.browser": true,
-                "process.env.NODE_ENV": JSON.stringify(mode)
-            }),
-            svelte({
-                ...svelteOptions,
-                dev,
-                hydratable: true,
-                emitCss: true,
-            }),
-            resolve({
-                browser: true
-            }),
-            commonjs(),
-            typescript(),
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      }),
+      svelte({
+        ...svelteOptions,
+        dev,
+        hydratable: true,
+        emitCss: true
+      }),
+      resolve({
+        browser: true
+      }),
+      commonjs(),
+      typescript(),
 
-            legacy &&
-                babel({
-                    extensions: [".js", ".mjs", ".html", ".svelte"],
-                    runtimeHelpers: true,
-                    exclude: ["node_modules/@babel/**"],
-                    presets: [
-                        [
-                            "@babel/preset-env",
-                            {
-                                targets: "> 0.25%, not dead"
-                            }
-                        ]
-                    ],
-                    plugins: [
-                        "@babel/plugin-syntax-dynamic-import",
-                        [
-                            "@babel/plugin-transform-runtime",
-                            {
-                                useESModules: true
-                            }
-                        ]
-                    ]
-                }),
+      legacy &&
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          runtimeHelpers: true,
+          exclude: ["node_modules/@babel/**"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: "> 0.25%, not dead"
+              }
+            ]
+          ],
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true
+              }
+            ]
+          ]
+        }),
 
-            !dev &&
-                terser({
-                    module: true
-                })
-        ],
+      !dev &&
+        terser({
+          module: true
+        })
+    ],
 
-        onwarn
-    },
+    onwarn
+  },
 
-    server: {
-        input: config.server.input(),
-        output: config.server.output(),
-        plugins: [
-            replace({
-                "process.browser": false,
-                "process.env.NODE_ENV": JSON.stringify(mode)
-            }),
-            svelte({
-                ...svelteOptions,
-                generate: "ssr",
-                dev,
-            }),
-            resolve(),
-            commonjs(),
-            typescript()
-        ],
-        external: Object.keys(pkg.dependencies).concat(
-            require("module").builtinModules ||
-                Object.keys(process.binding("natives"))
-        ),
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      replace({
+        "process.browser": false,
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      }),
+      svelte({
+        ...svelteOptions,
+        generate: "ssr",
+        dev
+      }),
+      resolve(),
+      commonjs(),
+      typescript()
+    ],
+    external: Object.keys(pkg.dependencies).concat(
+      require("module").builtinModules ||
+        Object.keys(process.binding("natives"))
+    ),
 
-        onwarn
-    },
+    onwarn
+  },
 
-    serviceworker: {
-        input: config.serviceworker.input(),
-        output: config.serviceworker.output(),
-        plugins: [
-            resolve(),
-            replace({
-                "process.browser": true,
-                "process.env.NODE_ENV": JSON.stringify(mode)
-            }),
-            commonjs(),
-            !dev && terser()
-        ],
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      }),
+      commonjs(),
+      !dev && terser()
+    ],
 
-        onwarn
-    }
+    onwarn
+  }
 };
